9f54a810f1c2edcf4d4bb1d165a893807fe6ecb4
Luke Withers <128609004+Lwithers93@users.noreply.github.com>
2023-11-15 16:37:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ function updateUI() {
         <i class="fa-regular fa-pen-to-square"></i>
       </button>
       <button class="iconBtn delete" onclick="deleteTodo(${todoIndex})"><i class="fa-solid fa-trash"></i></button>
-      <button class="iconBtn complete" onclick="completeTodo(${todoElement}, ${todoIndex})><i class="fa-solid fa-check"></i></button>
+      <button class="iconBtn complete" onclick="completeTodo(${todoElement}, ${todoIndex})"><i class="fa-solid fa-check"></i></button>
     </div>
     </div>`;
   });
